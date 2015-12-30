97bd4c1c994ecb5cb499318ec346751450b7385e
shiga yuichi <yuichi.shiga@gmail.com>
2015-12-29 18:36:47 -0800

    complete tutorial

---
+++ drone-tutorial/DTDrone.h
@@ -0,0 +1,34 @@
+#import <libARDiscovery/ARDISCOVERY_BonjourDiscovery.h>
+#import <libARController/ARCONTROLLER_Device.h>
+
+#pragma mark - interface
+@interface DTDrone : NSObject {
+}
+
+#pragma mark - properties
+@property (nonatomic) ARCONTROLLER_Device_t *deviceController;
+@property (nonatomic, assign) uint8_t batteryLevel;
+@property (nonatomic) dispatch_semaphore_t stateSem;
+
+#pragma mark - class method
++ (DTDrone *)sharedInstance;
+
+#pragma mark - public api
+/**
+ * connect drone
+ * @param service ARService
+ * @return success or failure BOOL
+ **/
+- (BOOL)connectWithService:(ARService *)service;
+
+/**
+ * disconnect drone
+ **/
+- (void)disconnect;
+
+- (void)emergency;
+- (void)takeoff;
+- (void)land;
+- (void)flip;
+
+@end
--- drone-tutorial/ViewController.swift
+++ drone-tutorial/ViewController.swift
@@ -7,19 +7,111 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var deviceNameLabel: UILabel!
+    @IBOutlet weak var takeOffButton: UIButton!
+    @IBOutlet weak var landButton: UIButton!
+    @IBOutlet weak var flipBtn: UIButton!
+    
+    let motionManager = CMMotionManager()
+    var isConnected = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        setControllButtonsHidden(true)
+        
+        registerReceivers()
+        startDiscovery()
+        
+        // 加速度の取得
+        motionManager.accelerometerUpdateInterval = 0.1
+        let accelerometerHandler:CMAccelerometerHandler = {
+            (data: CMAccelerometerData?, error: NSError?) -> Void in
+            
+            if self.isConnected && abs(data!.acceleration.z) > 3.5 {
+                DTDrone.sharedInstance().flip()
+            }
+        }
+        
+        motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue()!, withHandler: accelerometerHandler)
+    }
+    
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        unregisterReceivers()
+        stopDiscovery()
+        
+        if motionManager.accelerometerActive {
+            motionManager.stopAccelerometerUpdates()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func startDiscovery() {
+        ARDiscovery.sharedInstance().start()
+    }
+    
+    @IBAction func didTakeOffTap(sender: AnyObject) {
+        DTDrone.sharedInstance().takeoff()
+    }
+    
+    @IBAction func didLandTap(sender: AnyObject) {
+        DTDrone.sharedInstance().land()
+    }
+    
+    @IBAction func didFlipBtn(sender: AnyObject) {
+        DTDrone.sharedInstance().flip()
+    }
+    
+    func registerReceivers() {
+        NSNotificationCenter.defaultCenter().addObserver(
+            self,
+            selector: Selector("discoveryDidUpdateServices:"),
+            name: kARDiscoveryNotificationServicesDevicesListUpdated,
+            object: nil
+        )
+    }
+    
+    func discoveryDidUpdateServices(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            let deviceList = userInfo[kARDiscoveryServicesList]
+            
+            // 未接続状態なら、1つ目のデバイスに接続する
+            if isConnected == false {
+                let serviceList = deviceList as! [ARService]
+                isConnected = DTDrone.sharedInstance().connectWithService(serviceList[0])
+                
+                // UIの更新
+                setControllButtonsHidden(!isConnected)
+                deviceNameLabel.text = serviceList[0].name
+            }
+        }
+    }
+    
+    func setControllButtonsHidden(isHidden:Bool){
+        takeOffButton.hidden = isHidden
+        landButton.hidden = isHidden
+        flipBtn.hidden = isHidden
+    }
+    
+    func unregisterReceivers() {
+       NSNotificationCenter.defaultCenter().removeObserver(self, name: kARDiscoveryNotificationServicesDevicesListUpdated, object: nil)
+    }
+    
+    func stopDiscovery() {
+       ARDiscovery.sharedInstance().stop()
+    }
 }
-
